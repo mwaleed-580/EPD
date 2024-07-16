b080ddc9fa77a3f69639174d79dfbb42d0708763
Muhammad Waleed <waleedurahman@gmail.com>
2024-07-16 14:52:25 +0500

    Added config to ignore

--- auth/config.php
+++ auth/config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = '85.209.92.162';
 $username = 'remoteuser';
-$password = 'admin@db';
+$password = 'admin@db1';
 $dbname = 'epd1';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
